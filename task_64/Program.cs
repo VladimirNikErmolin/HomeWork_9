5b93ba0c2f7bf78cc2e077494c9d4f2b056e2d5b
Vladimir Ermolin <w.ermolin@yahoo.com>
2022-10-02 13:08:15 +0300

    task_64

---
+++ task_64/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.WriteLine("Введите число N:");
+int numberN = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Натуральный ряд от N : {GreatNaturalRow(numberN)}");
+
+string GreatNaturalRow(int n)
+{
+    if (n == 1) return "1";
+    return n + "," + GreatNaturalRow(n - 1);
+}
