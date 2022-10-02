15ac2145ad39de3cad5bcca1051639c1360b7b3e
Vladimir Ermolin <w.ermolin@yahoo.com>
2022-10-02 13:09:42 +0300

    task_68

---
+++ task_68/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int numberM = EnterInt("Введите число m:");
+int numberN = EnterInt("Введите число n:");
+
+Console.WriteLine($"Значение функции Аккермана для чисел m = {numberM} и n = {numberN} равно: {FindAckermannFunction(numberM, numberN)}");
+
+int EnterInt(string hint)
+{
+    Console.WriteLine(hint);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int FindAckermannFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return FindAckermannFunction(m - 1, 1);
+    }
+    else
+    {
+        return FindAckermannFunction(m - 1, FindAckermannFunction(m, n - 1));
+    }
+}
